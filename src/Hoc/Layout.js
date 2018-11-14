00d67be93c28191f0fd3f153c82817a3cf6fef0b
oro91dev <oro91dev@gmail.com>
2018-11-14 21:38:47 +0100

    manchester city

---
+++ src/Hoc/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Components/Header/Header';
+import Footer from '../Components/Footer/Footer';
+
+const Layout = (props) => {
+    return (
+        <div>
+            <Header/>
+            
+            {props.children}
+            <Footer/>
+        </div>
+    );
+};
+
+export default Layout;
