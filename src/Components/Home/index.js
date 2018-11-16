adea4b3c9975af4d2337492f4b6fe36eedf7d313
oro91dev <oro91dev@gmail.com>
2018-11-16 15:00:04 +0100

    added first section
    added animations and first section

---
+++ src/Components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Featured from './featured';
+import Text from './featured/Text';
+
+const Home = () => {
+    return (
+        <div className="bck_blue">
+            <Featured/>
+            <Text/>
+        </div>
+    );
+};
+
+export default Home; 
